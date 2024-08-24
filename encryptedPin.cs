64bf848e9366ae0b405bf3d5a3d60d6e5f091c15
Awdiini <bashirawdiini@gmail.com>
2024-08-24 14:26:08 +0300

    adding some files

---
+++ encryptedPin.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+
+public class PinEncryption
+{
+    public static string EncryptPin(string pin, string cardNumber, string publicKeyBase64)
+    {
+        try
+        {
+            // Create Hex PIN Block
+            string pinHex = pin.Length.ToString("X").PadLeft(2, '0') + pin.PadRight(15, 'F');
+            
+            // Create Hex Card Lock
+            string cardHex = "0000" + cardNumber.Substring(cardNumber.Length - 12);
+            
+            // Perform XOR operation
+            long pinDecimal = Convert.ToInt64(pinHex, 16);
+            long cardDecimal = Convert.ToInt64(cardHex, 16);
+            long xorResult = pinDecimal ^ cardDecimal;
+            
+            string xorHex = xorResult.ToString("X").PadLeft(16, '0');
+            
+            // Convert Hex Result to Byte Array
+            byte[] bytes = new byte[xorHex.Length / 2];
+            for (int i = 0; i < xorHex.Length; i += 2)
+            {
+                bytes[i / 2] = Convert.ToByte(xorHex.Substring(i, 2), 16);
+            }
+            
+            // Load and parse the public key
+            byte[] publicKeyBytes = Convert.FromBase64String(publicKeyBase64);
+            Asn1Object asn1Object;
+            using (var stream = new Asn1InputStream(publicKeyBytes))
+            {
+                asn1Object = stream.ReadObject();
+            }
+            
+            var rsaKeyParameters = PublicKeyFactory.CreateKey(SubjectPublicKeyInfo.GetInstance(asn1Object)) as RsaKeyParameters;
+            
+            var rsaParams = new RSAParameters
+            {
+                Modulus = rsaKeyParameters.Modulus.ToByteArrayUnsigned(),
+                Exponent = rsaKeyParameters.Exponent.ToByteArrayUnsigned()
+            };
+            
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                rsa.ImportParameters(rsaParams);
+                
+                // Use PKCS#1 v1.5 padding if that’s the vendor’s requirement
+                byte[] encryptedBytes = rsa.Encrypt(bytes, RSAEncryptionPadding.Pkcs1);
+                
+                return Convert.ToBase64String(encryptedBytes);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+            return null;
+        }
+    }
+}
---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example values
+            string pin = "1234"; // Your PIN
+            string cardNumber = "5213636125746050"; // Your card number
+            string publicKeyBase64 = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAt8MFxB0tQiBpIXLwMYQOF35JqN4gDqKjLPvlwtBzj+iyo/ogXDkVex1ls8xEe4/gC68Lf/LpkUVORo31yOvW3MYkGEZ/xzwF8+5fxQsNJb/bA0xpNtikN0FaEYqz/FTpn2r0vgB4Km3K5fbcMfjDyIw1YUWQZc0ShSPjO959lEUvRddoIXyzrRGbp8aigwye7dHwumFzeXcmGNX6TY3TZ6qU0p7ol6pS4XSeNIsW93tT62WvNAcOOgs99WXQMEVlZ9IYnig9c6qUOLyHcG6JJF0PyeH2FWLqBoT17b575uBrUC93FQ9Nba5trJBinZSQXzV6RoyHuLU/z9ZIGdySxQIDAQAB"; // Base64 encoded public key
+
+            string encryptedPin = PinEncryption.EncryptPin(pin, cardNumber, publicKeyBase64);
+            Console.WriteLine("Encrypted PIN: " + encryptedPin);
+        }
+    }
+}
